78f0a8794f16599efa70d0c9741d01273284551a
Aen <hello@aentan.com>
2019-12-20 13:28:57 +0800

    device detection, optimize assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return "win";
+  }
+
+  if (/android/i.test(userAgent)) {
+    return "android";
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "ios";
+  }
+
+  return "unknown";
+}
+
 $(function () {
   $(".hero-backgrounds").slick({
     infinite: true,
@@ -10,4 +30,6 @@ $(function () {
     arrows: false,
     dots: false
   });
+
+  $(".download-link").addClass(getMobileOperatingSystem());
 });
